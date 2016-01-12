a26fc43373f843edefcc233608441860a226528b
Kevin Rutten <Kevin@KevinRutten.com>
2016-01-12 10:19:45 -0800

    Print OPENSSL version

---
+++ CheckOpenSSL.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+    <title>OpenSSL Version Note</title>
+  </head>
+  <body>
+    <br>
+    <?php
+      echo OPENSSL_VERSION_TEXT
+    ?>
+    <br>
+  </body>
+</html>
